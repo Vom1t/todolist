e46dc25ff52b6811fc8d1b4bf3042551b6d9a6b8
dev <rvotaisrvota@gmail.com>
2023-02-02 00:14:40 +0300

    login and profile view

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,11 @@
 
 from django.urls import path
 
-from .views import SignupView
+from .views import SignupView, LoginView, ProfileView
 
 urlpatterns = [
-    path('signup', SignupView.as_view(), name='signup'),
+    path('signup', SignupView.as_view(), name='signup-view'),
+    path('login', LoginView.as_view(), name='login-view'),
+    path('profile', ProfileView.as_view(), name='profile-view'),
 
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,35 @@
-from rest_framework import generics
+from django.contrib.auth import login, logout
+from rest_framework import generics, status, permissions
+from rest_framework.response import Response
 
-from core.serializers import CreateUserSerializer
+from core.models import User
+from core.serializers import CreateUserSerializer, LoginSerializer, ProfileSerializer
 
 
 class SignupView(generics.CreateAPIView):
     serializer_class = CreateUserSerializer
+
+
+class LoginView(generics.CreateAPIView):
+    serializer_class = LoginSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def perform_create(self, serializer):
+        login(request=self.request, user=serializer.save())
+
+
+class ProfileView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = ProfileSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user
+
+    def perform_destroy(self, instance):
+        logout(self.request)
